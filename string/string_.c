340acffbc54e2a5ed2b380e9eb1d5abba1283e4a
slonikmc <Akelka431431@gmail.com>
2022-02-26 15:48:05 +0300

    added find functions and tests

--- string/string_.c
+++ string/string_.c
@@ -5,6 +5,44 @@
 #include "string_.h"
 #include "stdio.h"
 
-void printHello() {
-    printf("Hello");
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
+
+char *find(char *begin, char *end, int ch) {
+    while (begin != end && *begin != ch)
+        begin++;
+
+    return begin;
+}
+
+char *findNonSpace(char *begin) {
+    while (isspace(*begin))
+        begin++;
+
+    return begin;
+}
+
+char *findSpace(char *begin) {
+    while (!isspace(*begin) && *begin != '\0')
+        begin++;
+
+    return begin;
+}
+
+char *findNonSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin != rend && isspace(*rbegin))
+        rbegin--;
+
+    return rbegin;
+}
+
+char *findSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin != rend && !isspace(*rbegin) )
+        rbegin--;
+
+    return rbegin;
 }
